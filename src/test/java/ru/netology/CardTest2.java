1fd3024a467f075e5af9b681900cbc64800fe690
Nikolay Martoplyas <kolyan94martoplyas@gmail.com>
2024-08-16 20:04:01 +0300

    fixing tests

--- src/test/java/ru/netology/CardTest2.java
+++ src/test/java/ru/netology/CardTest2.java
@@ -25,7 +25,7 @@ public class CardTest2 {
         form.$("[data-test-id=phone] input").setValue("+79881234567");
         form.$("[data-test-id=agreement]").click();
         $(".button_view_extra").click();
-        form.$(".input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        form.$("[data-test-id=name].input_invalid .input__sub").shouldHave(Condition.text("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
@@ -35,7 +35,7 @@ public class CardTest2 {
         form.$("[data-test-id=phone] input").setValue("+79881234567");
         form.$("[data-test-id=agreement]").click();
         $(".button_view_extra").click();
-        form.$("[data-test-id=name] .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+        form.$("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
     @Test
@@ -45,7 +45,7 @@ public class CardTest2 {
         form.$("[data-test-id=phone] input").setValue("123456789");
         form.$("[data-test-id=agreement]").click();
         $(".button_view_extra").click();
-        form.$("[data-test-id=phone] .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        form.$("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
     @Test
@@ -55,7 +55,7 @@ public class CardTest2 {
         form.$("[data-test-id=phone] input").setValue("");
         form.$("[data-test-id=agreement]").click();
         $(".button_view_extra").click();
-        form.$("[data-test-id=phone] .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+        form.$("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
     @Test
@@ -64,7 +64,7 @@ public class CardTest2 {
         form.$("[data-test-id=name] input").setValue("Николай");
         form.$("[data-test-id=phone] input").setValue("+79881234567");
         $(".button_view_extra").click();
-        form.$("[data-test-id=agreement]").shouldHave(Condition.cssValue("color", "rgba(255, 92, 92, 1)"));
+        form.$("[data-test-id=agreement].input_invalid .checkbox__text").shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
     }
 
 }
